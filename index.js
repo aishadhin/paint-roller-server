8cc78e7b167b9615c40920303f07002b3ee6f912
Ai Shadhin <aishadhin.ac@gmail.com>
2022-05-25 13:01:39 +0600

    get single data

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
-const { MongoClient, ServerApiVersion } = require('mongodb'); // Mongo
-const express = require('express')
-const app = express()
-require('dotenv').config();
-const cors = require('cors')
-const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb"); // Mongo
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-
 // Mongo
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nah5m.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-    try{
-        await client.connect();
-        const productCollection = client.db('ai_roller').collection('products');
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("ai_roller").collection("products");
 
-        // get data from db to a server url as api
-        app.get('/product' , async(req, res)=>{
-            const query = {};
-            const cursor = productCollection.find(query);
-            const products = await cursor.toArray();
-            res.send(products)
-        })
-    }
-    finally{
+    // get data from db to a server as api
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
 
-    }
+
+    // get single data by id from db to a server as api
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    });
+
+
+
+  } finally {
+  }
 }
 run().catch(console.dir);
 
-
-
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
